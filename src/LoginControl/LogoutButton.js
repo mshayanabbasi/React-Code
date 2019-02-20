be2e646732593b9a26d61f7ab6cf60c77c2d1053
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-02-20 16:06:45 +0500

    js file

---
+++ src/LoginControl/LogoutButton.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+const logoutButton = (props) => {
+    return(
+        <button onClick={props.clicked}>
+        Logout
+        </button>
+    )
+}
+export default logoutButton
