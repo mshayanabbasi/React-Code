a53f855ab67ed5a46922a7aa783a9264e24f5b76
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-02-20 10:35:10 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
-  // state = {
-  //   counter : 1
-  // }
   state = {
     persons : [
       {name: 'Max', age: 28},
@@ -15,16 +12,11 @@ class App extends Component {
     otherState : 'some other value',
     showPersons: false
   }
-  swithNameHandler = (newName) => {
-  //   // console.log('Was Clicked')
-  //   //DON'T DO THIS this.state.persons[0].name = 'Maxmillian'
-    this.setState({
-      persons: [
-        {name: newName, age:28},
-        {name: 'Manu', age: 29},
-        {name: 'Stephanie', age:27}
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice()
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({persons: persons})
   }
   changeNameHandler = (event) => {
     this.setState({
@@ -39,11 +31,6 @@ class App extends Component {
       const doesShow = this.state.showPersons
       this.setState({showPersons: !doesShow})
     }
-  // switchCounterHandler = () =>{
-  //   this.setState({
-  //     counter:  this.state.counter + 1
-  //   })
-  // }
   render() {
     const style = {
       backgroundColor: 'white',
@@ -52,94 +39,31 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
-    let persons = null;
-    if( this.state.showPersons ){
-      persons = 
-         ( 
-         <div>
-              {this.state.persons.map(person =>{
-                return <Person
-                name={person.name}
-                age={person.age}/>
-              })}
-          </div>
-         )
+    var persons = null
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+            click={() => this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age} />
+          })}
+        </div>
+      )
     }
-   
-    let person = null;
-    if(this.state.newPerson){
-         person=( <div>
-              <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}/>
-              <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.swithNameHandler.bind(this, 'Max!')}
-              change={this.changeNameHandler}>My Hobbies: Racing</Person>
-              <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}/>
-          </div>
-         )
-    }
-   
     return (
-      <div>
-         <button onClick={this.switchCounterHandler}>Count</button>
-          <p>{this.state.counter}</p>
-      </div>
 
       <div className="App">
         <h1>Hi, I am React App</h1>
         <p>This is really working</p>
-        <button onClick={this.swithNameHandler.bind(this, 'Maximillian')}>Switch Name</button> */}
-        <button 
-        style ={style}
-        onClick={() => this.swithNameHandler('Maximillian')}>Switch Name</button>
         <button 
         style ={style}
         onClick={this.togglePersonHandler}>Toggle Person</button>
-        {person}
-        {
-          this.state.newPerson === true ?
-            <div>
-                <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age}/>
-                <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.swithNameHandler.bind(this, 'Max!')}
-                change={this.changeNameHandler}>My Hobbies: Racing</Person>
-                <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age}/>
-            </div> : null
-        }
-
-
-        
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}/> 
-         <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.swithNameHandler.bind(this, 'Max!')}>My Hobbies: Racing</Person> 
-         <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.swithNameHandler.bind(this, 'Max!')}
-        change={this.changeNameHandler}>My Hobbies: Racing</Person>
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}/>
-       <Person title="My First Post"></Person>
+        {persons}
       </div>
     );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Hi I am React App'))
-  }
+    }
 }
 
 export default App;
