09426a34c941f9c2ef304473e4abe889508c9223
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-02-20 16:06:38 +0500

    js file

---
+++ src/LoginControl/LoginButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const loginbutton = (props) =>{
+    return (
+        <button onClick={props.clicked}>
+        Login
+        </button>
+    )
+}
+export default loginbutton
