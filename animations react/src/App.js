9cf1beecfc1af022be4fd1146d96e5aa8af0b342
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-10-12 19:15:28 +0500

    animations

--- animations react/src/App.js
+++ animations react/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Transition from 'react-transition-group/Transition'
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -7,7 +7,8 @@ import List from "./components/List/List";
 
 class App extends Component {
    state = {
-     modalIsOpen: false
+     modalIsOpen: false,
+     showBlock: false
    }
    showModal = () => {
      this.setState({
@@ -23,6 +24,23 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button
+          className="Button" 
+          onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>
+          Toggle
+          </button>
+          <br />
+          <Transition in={this.state.showBlock} timeout={1000} unmountOnExit mountOnEnter>
+            {state => (  
+              <div style={{
+              backgroundColor: 'red',
+              width: 100,
+              height: 100,
+              margin: 'auto',
+              transition: 'opacity 1s ease-out',
+              opacity: state === 'exiting' ? 0 : 1
+            }} /> ) }
+          </Transition>
         {this.state.modalIsOpen ? 
         <Modal show={this.state.modalIsOpen} closed={this.closedModal}/> : null
         }
